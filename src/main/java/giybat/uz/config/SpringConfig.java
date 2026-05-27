520c6e5141ad47bf1df4f3e03691ad31e5bb6076
Bobur Muxammadov <bmuxammadov1@gmail.com>
2026-05-27 22:06:30 +0500

    Security

--- src/main/java/giybat/uz/config/SpringConfig.java
+++ src/main/java/giybat/uz/config/SpringConfig.java
@@ -2,10 +2,68 @@ package giybat.uz.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.UUID;
+
 
 @Configuration
+@EnableWebSecurity
 public class SpringConfig {
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        // authentication - Foydalanuvchining identifikatsiya qilish.
+        // Ya'ni berilgan login va parolli user bor yoki yo'qligini aniqlash.
+        String password = UUID.randomUUID().toString();
+        System.out.println("User Password Mazgi: " + password);
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}" + password)
+                .roles("USER")
+                .build();
+
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(new InMemoryUserDetailsManager(user));
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+            authorizationManagerRequestMatcherRegistry
+                    .requestMatchers("/auth/**").permitAll()
+                    .anyRequest()
+                    .authenticated();
+        });
+
+        http.csrf(AbstractHttpConfigurer::disable); // csrf ochirilgan
+        http.cors(httpSecurityCorsConfigurer -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+            configuration.setAllowedMethods(Arrays.asList("*"));
+            configuration.setAllowedHeaders(Arrays.asList("*"));
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            httpSecurityCorsConfigurer.configurationSource(source);
+        });
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
